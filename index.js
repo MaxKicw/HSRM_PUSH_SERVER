9b1e0b49fb25dba5c4450534c6f2cc69f25c649c
MaxKicw <m142679@googlemail.com>
2018-10-30 15:54:04 +0100

    Deleted cors :+1:

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 5000
-const cors = require('cors')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Content = require('./model/content');
@@ -16,7 +14,6 @@ mongoose.connect(url,{useMongoClient: true});
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use(cors());
 
 
 // app.post('/login',function(req,res){
